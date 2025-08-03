f4988fbad24403a655ee5ad0f61176621fa9ba38
AndyMasc <andy.mascarenhas12@gmail.com>
2025-08-04 10:10:49 +1200

    Change landing page div to grid

---
+++ HealthHub/DoctorWorkspace/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DoctorworkspaceConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'DoctorWorkspace'
---
+++ HealthHub/patientWorkspace/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PatientworkspaceConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'patientWorkspace'
