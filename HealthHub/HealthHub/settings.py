a44bb4d9d55cd6d9d5a9bea68121e786100d56cb
AndyMasc <andy.mascarenhas12@gmail.com>
2025-09-05 09:41:10 +1200

    allow all hosts

--- HealthHub/HealthHub/settings.py
+++ HealthHub/HealthHub/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['healthhub.koyeb.app', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 LOGIN_URL = "authenticate:signin"
 
