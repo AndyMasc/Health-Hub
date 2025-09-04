d80936fe78cf5c50a6aa836ff5b20ee8f83fb2ed
AndyMasc <andy.mascarenhas12@gmail.com>
2025-09-05 09:35:19 +1200

    Set debug true

--- HealthHub/HealthHub/settings.py
+++ HealthHub/HealthHub/settings.py
@@ -16,10 +16,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
-
-from django.conf.global_settings import SECRET_KEY
-
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -33,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['healthhub.koyeb.app', '127.0.0.1', 'localhost']
 
