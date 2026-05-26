1ae0bf845f1778484a6b870f8ec77d776db1cdf8
AndyMasc <andy.mascarenhas12@gmail.com>
2026-05-26 21:06:04 +1200

    formatting

--- HealthHub/home/views.py
+++ HealthHub/home/views.py
@@ -1,13 +1,14 @@
-from django.contrib.auth.models import User
-from django.shortcuts import render, redirect
 from authenticate.models import Account
+from django.contrib.auth.models import User
+from django.shortcuts import redirect, render
 
 # Create your views here.
 
+
 def index(request):
     if request.user.is_authenticated:
-        if request.user.account.role == 'Patient':
-            return redirect('workspace:dashboard')
-        elif request.user.account.role == 'Doctor':
-            return redirect('workspace:dashboard')
-    return render(request, 'home/index.html')
+        if request.user.account.role == "Patient":
+            return redirect("workspace:dashboard")
+        elif request.user.account.role == "Doctor":
+            return redirect("workspace:dashboard")
+    return render(request, "home/index.html")
