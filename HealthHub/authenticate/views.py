8295a32f8f85dce7fc76a0f40c3fce79fe5d417b
AndyMasc <andy.mascarenhas12@gmail.com>
2026-05-26 21:07:29 +1200

    format

--- HealthHub/authenticate/views.py
+++ HealthHub/authenticate/views.py
@@ -2,111 +2,126 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import PermissionDenied
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect, render
 
-from .forms import CreateUserForm, AuthorizeUser, UpdateUser, AddPatientForm
-from .models import Patient, Doctor, Account
-
+from .forms import AddPatientForm, AuthorizeUser, CreateUserForm, UpdateUser
+from .models import Doctor, Patient
 
 # Create your views here.
+
 
 @login_required
 def account(request):
     form = UpdateUser(instance=request.user)
-    context = {'form': form}
-    return render(request, 'authenticate/account.html', context)
+    context = {"form": form}
+    return render(request, "authenticate/account.html", context)
+
 
 @login_required
 def update_user(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = UpdateUser(request.POST, instance=request.user)
         if form.is_valid():
             form.save()
-            messages.success(request, 'Your profile was updated successfully')
-            return redirect('authenticate:account')
+            messages.success(request, "Your profile was updated successfully")
+            return redirect("authenticate:account")
         else:
             for field, errors in form.errors.items():
                 for error in errors:
                     messages.error(request, f"{error}")
     else:
         form = UpdateUser(instance=request.user)
-    return render(request, 'authenticate/account.html', {'form': form})
+    return render(request, "authenticate/account.html", {"form": form})
+
 
 def register(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = CreateUserForm(request.POST)
-        context = {'form': form}
+        context = {"form": form}
         if form.is_valid():
             form.save()
-            user = authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password1'])
-            role = form.cleaned_data['role']
-            if role == 'Doctor':
+            user = authenticate(
+                username=form.cleaned_data["username"],
+                password=form.cleaned_data["password1"],
+            )
+            role = form.cleaned_data["role"]
+            if role == "Doctor":
                 Doctor.objects.create(user=user)
-            elif role == 'Patient':
+            elif role == "Patient":
                 Patient.objects.create(user=user)
             login(request, user)
-            return redirect('workspace:dashboard')
+            return redirect("workspace:dashboard")
     else:
         form = CreateUserForm()
-        context = {'form': form}
-    return render(request, 'authenticate/register.html', context)
+        context = {"form": form}
+    return render(request, "authenticate/register.html", context)
+
 
 def signin(request):
     form = AuthorizeUser()
-    context = {'form': form}
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+    context = {"form": form}
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('home:index')
+            return redirect("home:index")
         else:
-            messages.error(request, 'Invalid credentials. Please try again.')
-    return render(request, 'authenticate/signin.html', context)
+            messages.error(request, "Invalid credentials. Please try again.")
+    return render(request, "authenticate/signin.html", context)
+
 
 @login_required()
 def signout(request):
     logout(request)
-    return redirect('home:index')
+    return redirect("home:index")
+
 
 @login_required()
 def delete_account(request):
     user = request.user
     user.delete()
-    messages.success(request, 'Your account has been deleted successfully.')
-    return redirect('authenticate:signin')
+    messages.success(request, "Your account has been deleted successfully.")
+    return redirect("authenticate:signin")
+
 
 @login_required()
 def add_patient(request):
     form = AddPatientForm()
-    context = {'form': form}
-    if request.method == 'POST':
-        patient_user = request.POST.get('patient_user')
-        patient_password = request.POST.get('patient_password')
+    context = {"form": form}
+    if request.method == "POST":
+        patient_user = request.POST.get("patient_user")
+        patient_password = request.POST.get("patient_password")
         patient_obj = authenticate(username=patient_user, password=patient_password)
 
         if patient_obj is not None:
             try:
                 patient = Patient.objects.get(user=patient_obj)
             except Patient.DoesNotExist:
-                messages.error(request,'This user is not registered as a patient. Register a valid patient account.')
-                return render(request, 'authenticate/add_patient.html', {'form': form})
+                messages.error(
+                    request,
+                    "This user is not registered as a patient. Register a valid patient account.",
+                )
+                return render(request, "authenticate/add_patient.html", {"form": form})
 
             doctor = Doctor.objects.get(user=request.user)
             patient.doctor.add(doctor)
             patient.save()
-            return redirect('workspace:patients_view')
+            return redirect("workspace:patients_view")
         else:
-            messages.error(request, 'Invalid credentials. Please try again.')
-    return render(request, 'authenticate/add_patient.html', {'form': form})
+            messages.error(request, "Invalid credentials. Please try again.")
+    return render(request, "authenticate/add_patient.html", {"form": form})
+
 
 @login_required()
 def remove_patient(request, patient_id):
     patient = Patient.objects.get(id=patient_id)
-    if (request.user.account.role == 'Doctor') and patient.doctor.filter(user=request.user).exists():
+    if (request.user.account.role == "Doctor") and patient.doctor.filter(
+        user=request.user
+    ).exists():
         patient.doctor.remove(request.user.doctor)
         patient.save()
-        return redirect('workspace:patients_view')
+        return redirect("workspace:patients_view")
     else:
         raise PermissionDenied
