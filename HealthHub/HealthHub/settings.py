b549355a4955b861a3872ee70a8c189bcece3d68
Kelly Mascarenhas <kellymascarenhas@Kellys-MacBook-Air-4.local>
2026-06-06 20:07:29 +1200

    add trusted origins

--- HealthHub/HealthHub/settings.py
+++ HealthHub/HealthHub/settings.py
@@ -25,6 +25,7 @@ ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 LOGIN_URL = "authenticate:signin"
 
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+CSRF_TRUSTED_ORIGINS = ["https://healthhub.koyeb.app"]
 
 # Security settings (enable when ready for production)
 if not DEBUG:
